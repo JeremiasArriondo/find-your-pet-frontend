958d11b374689a5bcbe8c488307bea3d27c1a937
JeremiasArriondo <jeremiasarriondo98@gmail.com>
2022-06-10 14:40:07 -0300

    Creando custom hook useForm

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,23 +1,85 @@
+import { Button } from '@nextui-org/react';
+import useForm from '../../hooks/useForm';
 import styles from './Register.module.css';
 
 const Register = () => {
-  return (
-    <div className={styles['container']}>
-        <form className={styles['container-register']}>
-            <h1>Registro</h1>
-            <label for='name'>Nombre: </label>
-            <input type={'text'} name='name' placeholder='Ingresa tu nombre'/>
-            <label for='lastName'>Apellido: </label>
-            <input type={'text'} placeholder='Ingresa tu apellido'/>
-            <label for='email'>Email: </label>
-            <input type={'email'} placeholder='Ingresa tu correo'/>
-            <label for='password'>Contraseña: </label>
-            <input type='password' placeholder='Ingresá tu contraseña' />
-            <label for='password'>Contraseña: </label>
-            <input type='password' placeholder='Repetí la contraseña' />
 
-        </form>
-    </div>
+    const [ formValues, handleInputCHange] = useForm({
+        name: '',
+        lastName: '',
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    });
+
+    const { name,lastName, email, password, passwordConfirm } = formValues;
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log( formValues )
+    }
+
+    return (
+        <div className={styles['container']}>
+            <form className={styles['container-register']} onSubmit={ handleSubmit } >
+                <h1>Registro</h1>
+                <label htmlFor='Name'>Nombre: </label>
+                <input
+                    id='Name'
+                    type='text'
+                    name='name'
+                    minLength={3}
+                    placeholder='Ingresa tu nombre'
+                    value={ name }
+                    onChange={ handleInputCHange }    
+                />
+                <label htmlFor='LastName'>Apellido: </label>
+                <input
+                    id='LastName'
+                    type='text'
+                    name='lastName'
+                    minLength={3}
+                    placeholder='Ingresá tu apellido'
+                    value={ lastName }
+                    onChange={ handleInputCHange }
+                />
+                <label htmlFor='Email'>Email: </label>
+                <input
+                    id='Email'
+                    type='email'
+                    name='email'
+                    required={true}
+                    pattern= "(\w|^)[\w.\-]{0,25}@(hotmail|gmail)\.com"
+                    placeholder='tucorreo@email.com'
+                    value={ email }
+                    title='Por favor, ingrese una dirección de correo válida'
+                    onChange={ handleInputCHange }
+                />
+                <label htmlFor='Password'>Contraseña: </label>
+                <input 
+                    id='Password'
+                    type='password'
+                    name='password'
+                    placeholder='Ingresá tu contraseña'
+                    autoComplete='off'
+                    minLength={6}
+                    value={ password }
+                    onChange={ handleInputCHange }
+                />
+                <label htmlFor='passwordConfirm'>Contraseña: </label>
+                <input
+                    id='passwordConfirm'
+                    type='password'
+                    name='passwordConfirm'
+                    placeholder='Repetí la contraseña'
+                    minLength={6}
+                    autoComplete='off'
+                    value={ passwordConfirm }
+                    onChange={ handleInputCHange }
+                />
+                <Button style={{width: '100%'}} type='submit'>Registrar</Button>
+            </form>
+        </div>
   )
 }
 
